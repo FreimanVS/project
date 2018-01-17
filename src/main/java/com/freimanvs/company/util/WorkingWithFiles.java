0f67d7ba498136ecf14ce25466391867aebddc7d
FreimanVS <freimanvs@gmail.com>
2018-01-17 20:41:40 +0300

    file manager class is updated

--- src/main/java/com/freimanvs/company/util/WorkingWithFiles.java
+++ src/main/java/com/freimanvs/company/util/WorkingWithFiles.java
@@ -1,16 +1,41 @@
 package com.freimanvs.company.util;
 
 import java.io.*;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class WorkingWithFiles {
-    private static void createNewFile(File file) {
-        if(!file.exists()){
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
+    public static void writeToFile(String what, String where) {
+        writeToFile(what, Paths.get(where));
+    }
+
+    public static void writeToFile(String what, Path where) {
+        try (BufferedWriter bw = Files.newBufferedWriter(where)) {
+            bw.write(what);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*try (OutputStream os = Files.newOutputStream(Paths.get(where));
+             OutputStreamWriter osw = new OutputStreamWriter(os)) {
+            osw.write(what);
+            osw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    public static void readFromFile(String path) {
+        readFromFile(Paths.get(path));
+    }
+
+    public static void readFromFile(Path path) {
+        try (BufferedReader br = Files.newBufferedReader(path)) {
+            br.lines().forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -23,6 +48,16 @@ public class WorkingWithFiles {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void createNewFile(File file) {
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
