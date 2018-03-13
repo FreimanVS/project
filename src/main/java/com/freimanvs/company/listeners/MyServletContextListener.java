e8316a026b7e44f331fec3e55fcccf684cfdd4f5
FreimanVS <freimanvs@gmail.com>
2018-03-13 20:34:34 +0300

    waiting
    #waiting until db is ready is added

--- src/main/java/com/freimanvs/company/listeners/MyServletContextListener.java
+++ src/main/java/com/freimanvs/company/listeners/MyServletContextListener.java
@@ -9,14 +9,34 @@ import javax.servlet.annotation.WebListener;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
+import java.sql.*;
 
 @WebListener
 public class MyServletContextListener implements ServletContextListener {
 
     private static final String TEST_DATA_FILE_LOCATION = "DB_XML_location";
 
+    private static final String HOST = "192.168.99.100";
+    private static final String URL = "jdbc:mysql://" + HOST + ":3306/sys?useSSL=false";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "pass";
+
     @Override
     public void contextInitialized(ServletContextEvent arg0) {
+
+        //to sleep until database is ready
+        while (!connIsOk()) {
+            System.out.printf("DB is not ready yet or incorrect data:\r\n" +
+                    "ULR: %s\r\n" +
+                    "LOGIN: %s\r\n" +
+                    "PASSWORD: %s\r\n", URL, LOGIN, PASSWORD);
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
         //XML to DB
         try {
             System.out.println("XML to DB process...");
@@ -53,6 +73,30 @@ public class MyServletContextListener implements ServletContextListener {
 
         } catch (Exception e) {
             throw new RuntimeException("hibernate factory was not closed");
+        }
+    }
+
+    private static boolean connIsOk() {
+        try {
+            String driver = "com.mysql.jdbc.Driver";
+            Class.forName(driver);
+        } catch (Exception e) {
+            return false;
+        }
+
+        try (Connection conn = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+             Statement statement = conn.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT 1")) {
+
+            StringBuilder sb = new StringBuilder();
+            while(resultSet.next()){
+                sb.append(resultSet.getInt("1"));
+            }
+
+            return "1".equals(sb.toString());
+
+        } catch (SQLException e) {
+            return false;
         }
     }
 }
